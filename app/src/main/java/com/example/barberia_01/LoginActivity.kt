57780a890db79c6a51ba0c70e2f2a932e21c3eab
Richi <descargasnyx@gmail.com>
2026-02-06 19:38:35 -0500

    HASTA PANTALLA REGISTER APPOINTMENT TERMINADA

--- app/src/main/java/com/example/barberia_01/LoginActivity.kt
+++ app/src/main/java/com/example/barberia_01/LoginActivity.kt
@@ -13,15 +13,11 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
 
-        // Aquí obtenemos las referencias a los elementos visuales que creamos en el archivo .xml
-        // Usamos 'findViewById' para encontrar cada vista por su 'id' único.
         val usernameEditText = findViewById<TextInputEditText>(R.id.et_username)
         val passwordEditText = findViewById<TextInputEditText>(R.id.et_password)
         val loginButton = findViewById<Button>(R.id.btn_login)
 
 
-        // 'setOnClickListener' es como decirle a la app: "Oye, cuando alguien haga clic en este botón,
-        // ejecuta el código que está aquí dentro".
         loginButton.setOnClickListener {
             // Obtenemos el texto que el usuario escribió y lo convertimos a un String.
             val username = usernameEditText.text.toString()
@@ -29,19 +25,16 @@ class LoginActivity : AppCompatActivity() {
 
 
             if (username == "admin" && password == "1234") {
-                // Si las credenciales son correctas, creamos un 'Intent'.
-                // Un 'Intent' es una "intención" de hacer algo. En este caso, la intención
-                // es "ir desde esta pantalla (this) a la pantalla MainActivity".
+
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
 
-                // 'finish()' cierra la pantalla de login. De esta manera, si el usuario
-                // presiona el botón "atrás" en la pantalla principal, no volverá al login.
+
                 finish()
             } else {
-                // Si las credenciales son incorrectas, mostramos un mensaje de error temporal
-                // en la parte inferior de la pantalla, llamado 'Toast'.
-                Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show()
+
+                Toast.makeText(this, "Usuario o contraseña incorrectos",
+                    Toast.LENGTH_SHORT).show()
             }
         }
     }
--- app/src/main/java/com/example/barberia_01/RegisterAppointmentActivity.kt
+++ app/src/main/java/com/example/barberia_01/RegisterAppointmentActivity.kt
@@ -1,11 +1,20 @@
 package com.example.barberia_01
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class RegisterAppointmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_appointment)
+
+        val buttonConfirmAppointment = findViewById<Button>(R.id.buttonConfirmAppointment)
+
+        buttonConfirmAppointment.setOnClickListener {
+            Toast.makeText(this, "AUN NO CREO ESTA PANTALLA XD", Toast.LENGTH_SHORT).show()
+    }
+
     }
 }
